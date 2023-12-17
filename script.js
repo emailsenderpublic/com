1612abf69df38723ae14cb83cc63119a0d2b6d10
emailsenderpublic <154078811+emailsenderpublic@users.noreply.github.com>
2023-12-17 22:01:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_qvjfltp","template_z9os4e4",parms).then(alert("Email Sent!!"))
+}
